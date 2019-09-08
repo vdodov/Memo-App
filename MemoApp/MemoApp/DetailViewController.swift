8fbf63c3fd60d6ff925a821fa5287bb47d306319
chasuyeon <sy9600@naver.com>
2019-09-08 22:20:05 +0900

    메모 상세보기

---
+++ MemoApp/MemoApp/DetailViewController.swift
@@ -0,0 +1,52 @@
+//
+//  DetailViewController.swift
+//  MemoApp
+//
+//  Created by 차수연 on 08/09/2019.
+//  Copyright © 2019 차수연. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
+extension DetailViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 2
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    switch indexPath.row {
+    case 0:
+      let cell = tableView.dequeueReusableCell(withIdentifier: "memoCell", for: indexPath)
+      return cell
+    case 1:
+      let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell", for: indexPath)
+      return cell
+    default:
+      fatalError()
+    }
+  }
+  
+  
+}
